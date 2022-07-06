d054c1b4176bb632441e7a9a3f6c64dc0f77b408
nico <2265364722@qq.com>
2022-07-06 10:06:44 +0800

    push commit

--- src/main/java/com.nico.git/GitTest.java
+++ src/main/java/com.nico.git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello master-test");
         System.out.println("hello hot-fix-test");
+        System.out.println("hello push-test");
 
 
     }
